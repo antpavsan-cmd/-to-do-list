5d39708bc1e4cc1ede8defa9529d74d47bb01dae
antpavsan-cmd <antpavsan@gmail.com>
2026-03-15 21:40:32 +0500

    кнопка очистки,заголовки

--- main.py
+++ main.py
@@ -5,37 +5,43 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.checkbox import CheckBox
 from kivy.core.window import Window
-<<<<<<< HEAD
-
-=======
+from kivy.uix.scrollview import ScrollView
 import json
 import os
->>>>>>> e5835b8 (добавлен скролл, и еще куча всего)
 class App(App):
 	def build(self):
 		Window.clearcolor = (0.5,0.5,0.5,1)
-		self.button_adds = 0
+		self.scroll = ScrollView(size_hint=(1,1))
+		self.layout_label= BoxLayout(orientation="horizontal",spacing=10,padding=1,size_hint=(1,None),height=50)
+		self.layout_rows = BoxLayout(orientation="vertical",spacing=1,padding=1,size_hint=(1,None))
+		self.layout_rows.bind(minimum_height=self.layout_rows.setter("height"))
+		self.scroll.add_widget(self.layout_rows)
+		self.layout_top = BoxLayout(orientation="horizontal",spacing=10,padding=1,size_hint=(1,None),height=200)
 		self.layout_main = BoxLayout(orientation="vertical",spacing=10,padding=10)
 		self.button = Button(text="новый ряд",size_hint=(1,None),height=200)
-<<<<<<< HEAD
-		self.button.bind(on_press=self.button_pressed)
-		self.label_top = Label(text="заметки",size_hint=(1,None),height=50)
-		self.layout_main.add_widget(self.label_top,index=10)
-		self.layout_main.add_widget(self.button)
-		return self.layout_main
-	def button_pressed(self,instance):
-		new_layout = BoxLayout(orientation="horizontal",spacing=5,padding=5,size_hint=(1,None),height=200)
-		checkbox=CheckBox(size_hint=(None,None),width=100,height=200)
-		new_layout.add_widget(checkbox)
-		checkbox.bind(active=lambda checkbox,value: self.checkbox_pressed(new_layout,value))
-		new_layout.add_widget(TextInput(multiline=True))
-=======
 		self.button.bind(on_press=self.create_row)
-		self.label_top = Label(text="заметки",size_hint=(1,None),height=50)
-		self.layout_main.add_widget(self.label_top,index=10)
-		self.layout_main.add_widget(self.button)
+		self.clear = Button(text="очистить",size_hint=(1,None),height=200)
+		self.clear.bind(on_press=self.remove_all)
+		self.save_but = Button(text="сохранить",size_hint=(1,None),height=200)
+		self.save_but.bind(on_press=self.save)
+		self.label_top = Label(text="заметки",size_hint=(None,None),width=450,height=50)
+		self.text_label=(TextInput(text="новое название",multiline=False,size_hint=(None,None),width=450,height=70))
+		self.text_label.opacity = 0
+		self.text_label.disabled= True
+		self.checkbox_label=CheckBox(size_hint=(None,None),width=50,height=50)
+		self.checkbox_label.bind(active=self.change_label)
+		self.layout_label.add_widget(self.text_label)
+		self.layout_label.add_widget(self.label_top)
+		self.layout_label.add_widget(self.checkbox_label)
+		self.layout_top.add_widget(self.button)
+		self.layout_top.add_widget(self.clear)
+		self.layout_top.add_widget(self.save_but)
+		self.layout_main.add_widget(self.layout_label)
+		self.layout_main.add_widget(self.layout_top)
+		self.layout_main.add_widget(self.scroll)
 		self.save_load()
 		return self.layout_main
+		#сейв лоад
 	def save_load(self):
 		if not os.path.exists("data.json"):
 			return
@@ -43,24 +49,26 @@ class App(App):
 			data = json.load(f)
 		for task in data:
 			self.create_row(text=task["text"],done=task["done"])
-	def save(self):
+			#сейв
+	def save(self,instance=None):
 		data = [ ]
-		for row in self.layout_main.children:
+		for row in reversed(self.layout_rows.children):
 			if isinstance(row,BoxLayout):
-						text = " "
+						text = ""
 						done = False
 						for widget in row.children:
 							if isinstance(widget,CheckBox):
 								done = widget.active
 							if isinstance(widget,TextInput):
-								text = widget.text			
+								text = widget.text
 						data.append({
 						"text": text,
-						"done": done
+						"done": done,
 						})
 		with open("data.json","w",encoding = "utf-8") as f:
 			json.dump(data,f,indent = 4,ensure_ascii=False)
-	def create_row(self,instance=None,text=" ",done=False):
+		#создание рядов
+	def create_row(self,instance=None,text="",done=False):
 		new_layout = BoxLayout(orientation="horizontal",spacing=5,padding=5,size_hint=(1,None),height=200)
 		checkbox=CheckBox(size_hint=(None,None),width=100,height=200)
 		checkbox.active = done
@@ -68,37 +76,38 @@ class App(App):
 		checkbox.bind(active=lambda checkbox,value: self.checkbox_pressed(new_layout,value))
 		textinput = TextInput(text=text,multiline=True)
 		new_layout.add_widget(textinput)
->>>>>>> e5835b8 (добавлен скролл, и еще куча всего)
-		self.layout_main.add_widget(new_layout)
-		self.button_adds +=1
-		if self.button_adds >= 10:
-			self.layout_main.remove_widget(self.button)
-<<<<<<< HEAD
-	def checkbox_pressed(self,layout,value):
-		if value:
-			if not hasattr(layout,'button_del'):
-				layout.button_del =Button(text='удалить строку')
-=======
-		self.save()
+		self.layout_rows.add_widget(new_layout)
+		if done:
+			self.checkbox_pressed(new_layout,True)
 	def checkbox_pressed(self,layout,value):
 		if value:
 			if not hasattr(layout,'button_del'):
 				layout.button_del =Button(text='удалить строку',size_hint=(None,1),width=325)
->>>>>>> e5835b8 (добавлен скролл, и еще куча всего)
 			layout.button_del.bind(on_press=lambda instance: self.del_row(instance,layout))
 			layout.add_widget(layout.button_del)
 		else:
 			if hasattr(layout,'button_del'):
 				if layout.button_del in layout.children:
 					layout.remove_widget(layout.button_del)
+		#кнопка удалялка
 	def del_row(self,instance,layout):
-		self.button_adds -= 1
-		self.layout_main.remove_widget(layout)
-		if self.button_adds <= 10 and self.button not in self.layout_main.children:
-			self.layout_main.add_widget(self.button,index=9)
-<<<<<<< HEAD
-=======
+		self.layout_rows.remove_widget(layout)
 		self.save()
->>>>>>> e5835b8 (добавлен скролл, и еще куча всего)
+	# кнопка очистки
+	def remove_all(self,instance=None):
+		self.layout_rows.clear_widgets()
+		#заголовки
+	def change_label(self,instance,value):
+		if value:
+			self.label_top.opacity = 0
+			self.label_top.disabled= True
+			self.text_label.opacity = 1
+			self.text_label.disabled= False
+		else:
+			self.label_top.text=self.text_label.text
+			self.text_label.opacity = 0
+			self.text_label.disabled= True
+			self.label_top.opacity = 1
+			self.label_top.disabled= False
 if __name__ =='__main__':
 	App().run()
